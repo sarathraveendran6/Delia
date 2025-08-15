16b426fdd7f680e7dc65abe78e2b8c9e7ec343b6
sarath.raveendran <sarath.raveendran@thoughtspot.com>
2025-08-15 18:30:54 +0530

    minor change. removing unwanted imports

--- src/main/java/com/delia/cortex/models/TaskModel.java
+++ src/main/java/com/delia/cortex/models/TaskModel.java
@@ -1,14 +1,12 @@
 package com.delia.cortex.models;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-
-import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Entity
 @Table(name = "tasks")
